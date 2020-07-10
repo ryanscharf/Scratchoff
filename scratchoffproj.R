95f686a071599e3da983f750d59881ae361e14d5
Ryan Scharf <ryan.scharf@gmail.com>
2020-07-09 22:07:09 -0400

    change in website yields more information for each game
    multiple wins per game?
    can't calculate nubmer of tickets printed?

--- scratchoffproj.R
+++ scratchoffproj.R
@@ -3,15 +3,23 @@ library(rvest)
 library(jsonlite)
 library(janitor)
 library(lubridate)
+#library(taskscheduleR)
 
+
+###notes
+###set up taskscheduler
+###pull data > make table > make html file (knitr?) > send to wordpress
+##https://developer.wordpress.org/rest-api/
+##https://opencontent.org/blog/archives/5779
 
 url <- 'https://www.flalottery.com/remainingPrizes'
 xpath <- '//*[@id="scratch-offs"]/div/table'
 
 ending_games <- 'https://www.flalottery.com/endingGames' %>%
   xml2::read_html() %>%
-  html_node(xpath = '//*[@id="scratch-offs"]/div/table') %>%
-  html_table() %>% clean_names() %>%
+  #html_node(xpath = '//*[@id="scratch-offs"]/div/table') %>%
+  html_node(xpath = '//*[(@id = "scratch-offs")]') %>% html_nodes('table') %>%
+  html_table() %>% .[[1]] %>% clean_names() %>%
   mutate(
     last_day_to_sell = parse_date(last_day_to_sell, format = '%B %d, %Y'),
     last_day_to_redeem = parse_date(last_day_to_redeem, format = '%B %d, %Y')
@@ -45,3 +53,48 @@ games_enh <-
     percent_remaining = prizes_remaining / tot_prizes,
     ending = (game_number %in% ending_games$game_number)
   )
+
+
+game_url <-  'https://www.flalottery.com/scratch-offsGameDetails?gameNumber='
+#game_tbl <- 
+  
+calc_ev <- function(cost, odds_denomintor, win){
+  ev <- (win * 1/odds_denomintor) - (cost * (1-1/odds_denomintor))
+  return(ev)
+  }
+  
+  get_granular_info <- function(x){
+    text <- xml2::read_html(paste0(game_url,x)) %>%
+      html_nodes(xpath = '//p') %>% html_text()
+    overall_odds <- text[grepl('Overall Odds', text)]
+    overall_odds_d <- as.numeric(
+      stringr::str_extract(
+        overall_odds,
+        '(?<=Overall Odds: 1-in-).*'
+        )
+      )
+      
+    num_printed_tickets <- ((1-(1/overall_odds_d)) * sum(test$total_prizes)) + sum(test$total_prizes)
+    
+    ticket_price <- text[grepl('Ticket Price', text)]
+     ticket_price <- as.numeric(
+       stringr::str_extract(
+       ticket_price,
+       '(?<=Ticket Price:\\s\\$).*(?=L)'
+       )
+     )
+     
+    xml2::read_html(paste0(game_url,x)) %>%
+      html_node(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "column2", " " ))]') %>% 
+      html_nodes('table') %>% 
+      html_table() %>% .[[1]] %>% clean_names() %>%
+      mutate(
+        prize_amount = parse_number(prize_amount),
+        odds_denominator = parse_number(
+          str_remove(
+            odds_of_winning, 
+            '1-in-')
+          ),
+        odds_of_winning = 1/odds_denominator
+        )
+  }
