eff6c8b125cea67935e89a4798e5f2a68657632f
ryan scharf <ryan.scharf@gmail.com>
2026-01-19 14:26:24 -0500

    cleaned up stuff for postgres transition

--- app.R
+++ app.R
@@ -1,44 +1,50 @@
 library(shiny)
-library(DT)
-library(tidyverse)
-library(odbc)
-library(ggthemes)
-library(lubridate)
-library(zoo)
+library(DBI)
 library(pool)
-#library(dbplyr)
-library(glue)
-library(config)
-library(shinycssloaders)
+library(RPostgres)
+library(tidyverse)
 library(httr2)
+library(glue)
+library(lubridate)
+library(shinycssloaders)
+library(DT)
+library(ggthemes)
+library(zoo)
 
-#library(bs4Dash)
 source('utils.R')
 
 conn_args <- config::get("dataconnection")
-con <- dbPool(RMariaDB::MariaDB(),
-              host = conn_args$host, 
-              port = conn_args$port, 
-              username = conn_args$username,
-              password = conn_args$password, 
-              db = conn_args$db,
-              minSize = conn_args$minSize,
-              idleTimeout = conn_args$idleTimeout
+
+con <- dbPool(
+  RPostgres::Postgres(),
+  host = conn_args$host,
+  port = conn_args$port,
+  user = conn_args$username,
+  password = conn_args$password,
+  dbname = conn_args$db,
+  minSize = conn_args$minSize,
+  idleTimeout = conn_args$idleTimeout
 )
 
-AOdates <- #dbGetQuery
-  dbGetQuery(con, 'SELECT distinct AOdate
-FROM scratchoff.game_info order by aodate desc
-         limit 25')
+AOdates <- dbGetQuery(
+  con,
+  'SELECT DISTINCT "AOdate"
+FROM game_info 
+ORDER BY "AOdate" DESC
+LIMIT 25'
+)
+
+# Convert to vector for selectInput
+AOdates <- AOdates$AOdate
 
 # the dashboard -----------------------------------------------------------
 
-
-
-shinyApp(ui = fluidPage(
-  #javascript magic
-  tags$head(
-    tags$script(HTML("
+shinyApp(
+  ui = fluidPage(
+    #javascript magic
+    tags$head(
+      tags$script(HTML(
+        "
   function showModal(imgSrc) {
     var modal = document.getElementById('imageModal');
     var modalImg = document.getElementById('modalImage');
@@ -51,15 +57,17 @@ shinyApp(ui = fluidPage(
   }
 
   // Close modal if clicking outside the image
-  document.addEventListener('cglick', function(event) {
+  document.addEventListener('click', function(event) {
     var modal = document.getElementById('imageModal');
     var modalImg = document.getElementById('modalImage');
     if (event.target === modal) {
       closeModal();
     }
   });
-")),
-    tags$style(HTML("
+"
+      )),
+      tags$style(HTML(
+        "
   #imageModal {
     display: none;
     position: fixed;
@@ -86,126 +94,144 @@ shinyApp(ui = fluidPage(
     color: white;
     cursor: pointer;
   }
-"))
+"
+      ))
     ),
-  
-  # Modal div (Hidden by default)
-  tags$div(
-    id = "imageModal",
-    tags$img(id = "modalImage")
-  ),
-  
-  
-  tabsetPanel(
-    tabPanel("Games Overview", fluidRow(mainPanel(
-      withSpinner(DTOutput('game_overview_table'))
-    ))),
-    tabPanel("Game Information", fluidRow(
-      column(
-        3,
-        selectInput('aodates', 'Date: ', AOdates),
-        # selectInput(
-        #   'game_names',
-        #   'Game: ',
-        #   avaliable_games)
-        uiOutput('avaliable_games'),
-        numericInput('lag', 'Time span (months): ', value = 3)
+
+    # Modal div (Hidden by default)
+    tags$div(
+      id = "imageModal",
+      tags$img(id = "modalImage")
+    ),
+
+    tabsetPanel(
+      tabPanel(
+        "Games Overview",
+        fluidRow(mainPanel(
+          withSpinner(DTOutput('game_overview_table'))
+        ))
       ),
-      mainPanel(
-        withSpinner(plotOutput('prizes_left_graph')),
-        withSpinner(plotOutput('ev_change_graph')),
-        textOutput('test1')
+      tabPanel(
+        "Game Information",
+        fluidRow(
+          column(
+            3,
+            selectInput('aodates', 'Date: ', AOdates),
+            # selectInput(
+            #   'game_names',
+            #   'Game: ',
+            #   avaliable_games)
+            uiOutput('avaliable_games'),
+            numericInput('lag', 'Time span (months): ', value = 3)
+          ),
+          mainPanel(
+            withSpinner(plotOutput('prizes_left_graph')),
+            withSpinner(plotOutput('ev_change_graph')),
+            textOutput('test1')
+          )
+        )
       )
-    ))
-  )
-), 
-  
-  
+    )
+  ),
+
   server = function(input, output) {
-    
-    aodate <- reactive({input$aodates})
-    selected_game_number <- reactive({ input$game_names %>% str_extract('.+(?= -)') })
+    aodate <- reactive({
+      input$aodates
+    })
+    selected_game_number <- reactive({
+      input$game_names %>% str_extract('.+(?= -)')
+    })
     # selected_game_name <- reactive({ input$game_names %>% str_extract('(?<=- ).+') })
-    selected_lag <- reactive({input$lag})
+    selected_lag <- reactive({
+      input$lag
+    })
     game_info <- reactive({
+      aodate_val <- aodate()
       dbGetQuery(
         con,
-        paste0(
-          "SELECT distinct
-          a.game_number, b.game_name, a.expected_value_current
-          FROM 
-            scratchoff.game_summaries a
-          LEFT JOIN
-            scratchoff.game_info b ON a.game_number = b.game_number
-          WHERE
-              cast(a.AOdate as date) = CAST('",
-          aodate(),
-          "' AS DATE)
-          
-          ORDER BY expected_value_current DESC"
+        glue_sql(
+          'SELECT DISTINCT
+        a.game_number, b.game_name, a.expected_value_current
+      FROM 
+        game_summaries a
+      LEFT JOIN
+        game_info b ON a.game_number = b.game_number 
+          AND a."AOdate" = b."AOdate"
+      WHERE
+        CAST(a."AOdate" AS DATE) = CAST({aodate_val} AS DATE)
+      ORDER BY expected_value_current DESC',
+          .con = con
         )
       )
     })
-    
-    prize_data <- reactive({ #dbGetQuery
-      dbGetQuery(con,
-                 paste0("SELECT * FROM scratchoff.game_prizes where game_number = '",
-                        selected_game_number(),
-                        "' and aodate = '", aodate(), "'"))
+
+    prize_data <- reactive({
+      game_num <- selected_game_number()
+      aodate_val <- aodate()
+
+      dbGetQuery(
+        con,
+        glue_sql(
+          'SELECT * FROM game_prizes 
+       WHERE game_number = {game_num}
+       AND "AOdate" = {aodate_val}',
+          .con = con
+        )
+      )
     })
-    
-    
-    
-    
+
     # outputs -----------------------------------------------------------------
-    output$game_info_table <- 
+    output$game_info_table <-
       renderDT(
         DT::datatable({
           game_info()
         })
       )
-    
-    
-    output$test1 <- renderText({paste0('aodate = ', aodate(), '\nsgnumber = ', selected_game_number())})
-    
+
+    output$test1 <- renderText({
+      paste0('aodate = ', aodate(), '\nsgnumber = ', selected_game_number())
+    })
+
     output$avaliable_games <- renderUI({
-      
       selectInput(
         'game_names',
         'Game: ',
-        paste0(game_info()$game_number, ' - ', game_info()$game_name))
-      
+        paste0(game_info()$game_number, ' - ', game_info()$game_name)
+      )
     })
-    
+
     output$prizes_left_graph <- renderPlot({
       plot_prizes_left(prize_data())
     })
-    
+
     output$ev_change_graph <- renderPlot({
-      asofdate <-  aodate()
+      asofdate <- aodate()
       game_number <- selected_game_number()[1]
       lag <- selected_lag()
       # browser
-      tryCatch({
-        plot_ev_change(asofdate = asofdate, 
-                       game_number = game_number,
-                       con,
-                       lag = lag)
-      },  error = function(e) {""} # Leave this line as-is.
+      tryCatch(
+        {
+          plot_ev_change(
+            asofdate = asofdate,
+            game_number = game_number,
+            con,
+            lag = lag
+          )
+        },
+        error = function(e) {
+          ""
+        } # Leave this line as-is.
       )
-      
     })
-    
+
     output$game_overview_table = renderDT(
-      
       DT::datatable(
-        game_overview_table(aodate = aodate(), con = con, positive_ev = F), 
+        game_overview_table(aodate = aodate(), con = con, positive_ev = F),
         escape = FALSE,
-        options = list(lengthChange = FALSE,
-                       paging = FALSE)
-      ) %>% formatRound(columns=c('expected_value_current'), digits=2) %>%
-        formatPercentage(columns =c('percent_ev'), digits = 0)
+        options = list(lengthChange = FALSE, paging = FALSE)
+      ) %>%
+        formatRound(columns = c('expected_value_current'), digits = 2) %>%
+        formatPercentage(columns = c('percent_ev'), digits = 0)
     )
-    
   }
 )
